6defde2760821c459645d61fd01e7e664427e4ef
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2023-06-17 14:18:09 -0700

    test

--- app.py
+++ app.py
@@ -6,8 +6,15 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 
+st.title("Drug Research and Discovery")
 
-st.title("Toxicity of Test Compounds")
+compound = 0
+weight = 0
+toxicity = 0
+#data: compound {weight = 0, toxicity = 0}
+st.subheader('Compound Name')
+st.write(compound)
+
 
 
 
