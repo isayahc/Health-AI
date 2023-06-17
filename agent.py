ef15f94ba1a0a0b1681a43bcfc86796a32bc5ada
John Harvardalinayu03 <alinayu@college.harvard.edu>
2023-06-17 17:12:08 -0400

    agent with tools

--- agent.py
+++ agent.py
@@ -3,14 +3,13 @@ from langchain.agents import initialize_agent
 from langchain.agents import Tool
 from langchain.utilities import SerpAPIWrapper
 from langchain.memory import ConversationBufferMemory
-
-search = SerpAPIWrapper()
+import chemSearch
 
 tools = [
     Tool(
-        name="Search",
-        func=search.run,
-        description="Useful when you want to answer questions about current events or things found online"
+        name="Chemical Search",
+        func=chemSearch.chemSearch,
+        description="Useful when you want information about a chemical"
     ),
 ]
 
@@ -22,3 +21,21 @@ agent_chain = initialize_agent(
 
 while True:
     print("AI: " + agent_chain.run(input=input("Human: ")))
+
+
+# input output documentation 
+# Human: What can you tell me about this chemical: CC(=O)OC1=CC=CC=C1C(=O)O
+# > Entering new AgentExecutor chain...
+# {
+#     "action": "Chemical Search",
+#     "action_input": "CC(=O)OC1=CC=CC=C1C(=O)O"
+# }
+# Observation: This chemical has these properties: Molecular weight: 180.042258736, Polar Surface Area: 63.60000000000001, LogP: 1.3101
+# Thought: {
+#     "action": "Final Answer",
+#     "action_input": "The chemical CC(=O)OC1=CC=CC=C1C(=O)O has a molecular weight of 180.042258736, a polar surface area of 63.60000000000001, and a LogP of 1.3101."
+# }
+
+# > Finished chain.
+# AI: The chemical CC(=O)OC1 = CC = CC = C1C(=O)O has a molecular weight of 180.042258736, a polar surface area of 63.60000000000001, and a LogP of 1.3101.
+# Human:
---
+++ chemSearch.py
@@ -0,0 +1,2 @@
+def chemSearch(input):
+    return "This chemical has these properties: Molecular weight: 180.042258736, Polar Surface Area: 63.60000000000001, LogP: 1.3101"
