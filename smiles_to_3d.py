2807d8b184707f034eacae6aecc60df839113688
isayahc <isayahc>
2023-06-17 16:25:33 -0400

    3d version of smiles_to_3d

--- smiles_to_3d.py
+++ smiles_to_3d.py
@@ -1,43 +1,56 @@
 import streamlit as st
-import py3Dmol
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import Draw
+import imageio
+import os
+import glob
 
-def visualize_3d_structure(smiles):
-    # Create an RDKit molecule object from the SMILES string
-    mol = Chem.MolFromSmiles(smiles)
+# Define the SMILES string
+smiles = "CCO"
 
-    # Generate the 3D coordinates
-    mol = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol)
-    AllChem.UFFOptimizeMolecule(mol)
+# Create an RDKit molecule object from the SMILES string
+mol = Chem.MolFromSmiles(smiles)
 
-    # Read the 3D coordinates from the molecule
-    coords = mol.GetConformer().GetPositions()
+# Generate the 3D coordinates
+mol = Chem.AddHs(mol)
+AllChem.EmbedMolecule(mol)
+AllChem.MMFFOptimizeMolecule(mol)
 
-    # Create a py3Dmol view
-    view = py3Dmol.view(width=600, height=400)
-    view.addModel(Chem.MolToMolBlock(mol), "mol")
-    view.setStyle({"stick": {}})
-    view.zoomTo()
+# Get atom coordinates
+conf = mol.GetConformer()
+atom_coords = [conf.GetAtomPosition(i) for i in range(mol.GetNumAtoms())]
 
-    try:
-        # Render the py3Dmol view in Streamlit as HTML
-        html = view.render()
-        st.components.v1.html(html, width=600, height=400)
-    except TypeError:
-        st.write(html)
+# Get x, y, and z coordinates
+x = [coord.x for coord in atom_coords]
+y = [coord.y for coord in atom_coords]
+z = [coord.z for coord in atom_coords]
 
-# Streamlit app code
-def main():
-    st.title("3D Structure Visualization")
+# Create directory for temporary images
+os.makedirs("images", exist_ok=True)
 
-    # Get the SMILES input from the user
-    smiles_input = st.text_input("Enter the SMILES string")
+# Create temporary images for GIF
+for angle in range(0, 360, 2):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z)
+    ax.view_init(30, angle)
+    plt.savefig(f"images/temp{angle}.png")
+    plt.close()
 
-    if smiles_input:
-        # Call the visualization function with the user input
-        visualize_3d_structure(smiles_input)
+# Create GIF
+images = []
+for file_name in sorted(glob.glob("images/*.png")):
+    images.append(imageio.imread(file_name))
+imageio.mimsave('movie.gif', images)
 
-if __name__ == "__main__":
-    main()
+# Display the GIF using Streamlit
+st.image('movie.gif')
+
+# Delete temporary images
+for file_name in glob.glob("images/*.png"):
+    os.remove(file_name)
