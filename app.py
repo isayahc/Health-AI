6e9bd03e84f37e363e024fc28af1e6488e29c050
Amin <aminkhalif99@gmail.com>
2023-06-17 15:58:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ import pandas as pd
 
 st.title("Toxicity of Test Compounds")
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-
-df
 
 
 test_compound = st.text_input("Enter the Test Compound")
